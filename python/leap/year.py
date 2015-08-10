232881bda83def33af76dcb448242d355e8831a5
Sebastian Reuße <seb@wirrsal.net>
2015-08-10 17:58:28 +0200

    Update.

--- python/leap/year.py
+++ python/leap/year.py
@@ -1,9 +1,9 @@
-def divisible(n, m):
+def is_divisible(n, m):
 
     return n % m == 0
 
 
 def is_leap_year(year):
 
-    return divisible(year, 4) and \
-        (not divisible(year, 100) or divisible(year, 400))
+    return is_divisible(year, 4) and \
+        (not is_divisible(year, 100) or is_divisible(year, 400))
