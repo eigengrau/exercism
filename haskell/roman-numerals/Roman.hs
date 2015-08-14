e29962bfb585827595b2790f1bcc69a58fe3ce40
Sebastian Reuße <seb@wirrsal.net>
2015-08-14 22:33:32 +0200

    Update.

--- haskell/roman-numerals/Roman.hs
+++ haskell/roman-numerals/Roman.hs
@@ -1,5 +1,5 @@
-{-# LANGUAGE ScopedTypeVariables #-}
-{-# LANGUAGE UnicodeSyntax       #-}
+{-# LANGUAGE UnicodeSyntax #-}
+{-# LANGUAGE ViewPatterns  #-}
 
 module Roman (numerals) where
 
@@ -24,7 +24,7 @@ data RomanDigit = I | V | X | L | C | D | M
 
 -- The RomanDigit Enum instance is, of course, partial. This is in
 -- line with other base instances for Bounded types. For safety,
--- safe{succ,pred,toEnum} are provided.
+-- safe{succ,pred,toRoman} are provided.
 instance Enum RomanDigit where
 
   succ r = head $ dropWhile (≤r) romanDigits
@@ -36,8 +36,8 @@ instance Enum RomanDigit where
   enumFrom x = enumFromTo x maxBound
   enumFromTo x y
     | x ≡ y     = [x]
-    | x > y     = x : enumFromTo (pred x) y ∷ [RomanDigit]
-    | otherwise = x : enumFromTo (succ x) y ∷ [RomanDigit]
+    | x > y     = x : enumFromTo (pred x) y
+    | otherwise = x : enumFromTo (succ x) y
 
   enumFromThenTo = error "enumFromThenTo: undefined"           -- Feeling lazy.
 
@@ -77,22 +77,20 @@ numerals x = toRomanPositional =≪ positionalValues x
 -- or it must be either of the next two smaller digits.
 subtractiveForm ∷ Int → Maybe String
 subtractiveForm 0 = Nothing
-subtractiveForm x = do
-
-  guard $ isPositional x  -- Sanity check.
-  let (largerValue, remainValue) = toEnumGE x
-  largerDigit ← safeToEnum largerValue                   ∷ Maybe RomanDigit
-  guard $ remainValue ∈ 1 : pred2 largerValue
-  remainDigit ← safeToEnum remainValue                   ∷ Maybe RomanDigit
-  return $ show =≪ [remainDigit, largerDigit]
+subtractiveForm (validatePositional → x) = do
+      let (largerValue, remainValue) = toEnumGE x
+      largerDigit ← safeToRoman largerValue
+      guard $ remainValue ∈ pred2 largerValue
+      remainDigit ← safeToRoman remainValue
+      return $ show =≪ [remainDigit, largerDigit]
 
 
 additiveForm ∷ Int → String
 additiveForm 0 = ""
-additiveForm x = show smallerDigit ⧺ remainDigits
+additiveForm (validatePositional → x) = show smallerDigit ⧺ remainDigits
   where
     (smallerValue, remainValue) = toEnumLE x
-    smallerDigit = toEnum smallerValue                    ∷ RomanDigit
+    smallerDigit = toEnum smallerValue                        ∷ RomanDigit
     remainDigits = additiveForm remainValue
 
 
@@ -121,16 +119,13 @@ toEnumLE x = (converted, remainder)
     minRoman  = fromEnum (minBound ∷ RomanDigit)
 
 
--- | Return the two successors to some number.
--- The type Int → (Int, Int) would be more natural, and safer;
--- however, then one could not easily write «a ∈ pred2 b». Is there
--- some fancy abstraction for containment checks on tuples? Lens?
+-- | Return up to the two next predecessors to some number.
 pred2 ∷ Int → [Int]
-pred2 x = liftM fromEnum $ guard (x > 5) ≫ pred₁ ⧺ pred₂
+pred2 x = fromEnum <$> catMaybes [pred₁, pred₂]
     where
-      xʹ    = maybeToList (safeToEnum x)                         ∷ [RomanDigit]
-      pred₁ = fmap pred xʹ
-      pred₂ = fmap pred pred₁
+      xʹ    = safeToRoman x
+      pred₁ = safePred =≪ xʹ
+      pred₂ = safePred =≪ pred₁
 
 
 -- There is also prelude-safeenum, but that one replaces the Prelude
@@ -144,18 +139,20 @@ safePred ∷ (Eq α, Enum α, Bounded α) ⇒ α → Maybe α
 safePred x | x ≡ minBound = Nothing
            | otherwise    = Just (pred x)
 
-safeToEnum ∷ ∀α . (Bounded α, Enum α) ⇒ Int → Maybe α
-safeToEnum x
-    | x ≥ minBoundʹ ∧ x ≤ maxBoundʹ = Just (toEnum x)
-    | otherwise                     = Nothing
-    where
-      minBoundʹ = fromEnum (minBound ∷ α)
-      maxBoundʹ = fromEnum (maxBound ∷ α)
+-- Unfortunately, since RomanDibit is partial not only outside of the
+-- bounds of its corresponding Bounded instance, but has «holes», this
+-- function can’t be defined universally over all Bounded α, but is
+-- specific to RomanDigit.
+safeToRoman ∷ Int → Maybe RomanDigit
+safeToRoman = (`Map.lookup` toRomanValue)
 
 
 -- | Validate whether some number can be a positional value.
-isPositional ∷ Int → Bool
-isPositional x = x `rem` 10 ↑ pred (numDigits x) ≡ 0
+validatePositional ∷ Int → Int
+validatePositional x
+  | isPositional x = x
+  | otherwise      = error "validatePositional: invalid positional value"
+  where isPositional z = z `rem` 10 ↑ pred (numDigits z) ≡ 0
 
 
 -- | Return the number of digits in a number, if it were represented
