5c03f24ec7fd2d6b268c29acad80cf6c3b9e26f0
Sebastian Reuße <seb@wirrsal.net>
2015-08-31 17:05:12 +0200

    Update.

--- haskell/house/House.hs
+++ haskell/house/House.hs
@@ -1,9 +1,16 @@
 {-# LANGUAGE DeriveDataTypeable #-}
 {-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE DataKinds #-}
 {-# LANGUAGE DeriveGeneric #-}
 {-# LANGUAGE DeriveFoldable     #-}
 {-# LANGUAGE DeriveFunctor      #-}
 {-# LANGUAGE DeriveTraversable  #-}
+{-# LANGUAGE FlexibleContexts  #-}
+{-# LANGUAGE UndecidableInstances  #-}
+{-# LANGUAGE BangPatterns #-}
+{-# LANGUAGE KindSignatures  #-}
+{-# LANGUAGE TypeFamilies  #-}
+{-# LANGUAGE TemplateHaskell  #-}
 {-# LANGUAGE UnicodeSyntax      #-}
 {-# LANGUAGE ExistentialQuantification      #-}
 {-# LANGUAGE StandaloneDeriving      #-}
@@ -24,6 +31,9 @@ import           Prelude.Unicode
 import           Prelude.Unicode.SR
 import           Text.ParserCombinators.ReadP
 import GHC.Generics
+import Data.Singletons
+import Data.Singletons.TH
+import Debug.Trace
 
 
 rhyme = undefined
@@ -50,99 +60,69 @@ target = concat ∘ intersperse " " $ [
 -- A toy grammar framework. --
 ------------------------------
 
-data Phrase λ = Phrase Head (Phrase λ) (Phrase λ)
-              | Leaf Head λ
-deriving instance Show λ ⇒ Show (Phrase λ)
-deriving instance Functor (Phrase)
-deriving instance Traversable (Phrase)
-deriving instance Foldable (Phrase)
-deriving instance Generic (Phrase λ)
-deriving instance Data λ ⇒ Data (Phrase λ)
 
-data Head = S | N | V | C | P | A | D | Con deriving (Data, Show)
+singletons [d|
+    data Head = Det | N | P | V | I | Coord | Subord | Adj | Adv deriving Show
+ |]
 
 
-----------------------------------
--- Generic programming helpers. --
-----------------------------------
+data XP (head∷Head) = XP (XBar head)
+                    | ∀ (specifier∷Head) .
+                      XPₛ (Specifier) (XBar head)
+                    | XPₐ (XP head) (X Coord) (XP head)
+data XBar (head∷Head) = XBar (X head)
+                      | XBarₘ (X head) [Complement]
+                      | ∀ (adjunct∷Head) .
+                        XBarₐ (XBar head) (Adjunct adjunct)
+                      | ∀ (adjunct∷Head) .
+                        XBarₐʹ (Adjunct adjunct) (XBar head)
+data X (head∷Head) = X String
+
+data Complement =
+     ∀ (head∷Head) .  Complement₂ (XP   head)
+  |  ∀ (head∷Head) .  Complement₁ (XBar head)
+  |  ∀ (head∷Head) .  Complement₀ (X    head)
+
+data Specifier =
+    ∀ (head∷Head) .  Specifier₂ (XP   head)
+  | ∀ (head∷Head) .  Specifier₁ (XBar head)
+  | ∀ (head∷Head) .  Specifier₀ (X    head)
 
 
+data Adjunct (head∷Head) =
+    Adjunct₂ (XP   head)
+  | Adjunct₁ (XBar head)
+  | Adjunct₀ (X    head)
+    deriving Show
 
-------------------------------------
--- A toy grammar for this domain. --
-------------------------------------
+instance Show Complement where
+    show (Complement₂ x) = show x
+    show (Complement₁ x) = show x
+    show (Complement₀ x) = show x
 
-np ∷ ReadP (Phrase String)
-np = do
-  d ← option Nothing (Just ⦷ det)
-  h ← nbar +≫ conjp nbar
-  return $ maybe h (\d → Phrase N d h) d
+instance Show Specifier where
+    show (Specifier₂ x) = show x
+    show (Specifier₁ x) = show x
+    show (Specifier₀ x) = show x
 
-nbar ∷ ReadP (Phrase String)
-nbar = do
-  a ← option Nothing (Just ⦷ adj)
-  h ← noun
-  c ← option Nothing (Just ⦷ cp ⧻ pp ⧻ adjp)
-  let projection₁ = maybe h (Phrase N h) c
-      projection₂ = maybe projection₁ (\a → Phrase N a projection₁) a
-  return projection₂
+instance Show (XP head) where
+    show (XP  head)      =  show head
+    show (XPₛ spec head) = "[[" ⧺ show spec ⧺ "][" ⧺ show head ⧺ "]]"
+    show (XPₐ l j r)     = "[[" ⧺ show l ⧺ "]" ⧺ show j ⧺ "[" ⧺ show r ⧺ "]]"
 
-adjp ∷ ReadP (Phrase String)
-adjp = do
-  d ← option Nothing (Just ⦷ det)
-  b ← adjbar
-  return $ maybe b (\d → Phrase A d b) d
+instance Show (XBar head) where
+    show (XBar   head)       = show head
+    show (XBarₘ  head comps) = show head ⧺ " [" ⧺ (comps ≫= show) ⧺ "]"
+    show (XBarₐ  head adj)   = show head ⧺ " ⟨" ⧺ show adj        ⧺ "⟩"
+    show (XBarₐʹ adj head)   = "⟨" ⧺ show adj  ⧺ "⟩ " ⧺ show head
 
-adjbar ∷ ReadP (Phrase String)
-adjbar = adjbarʹ -- +≫ conjp adjbarʹ
-    where
-      adjbarʹ = do
-        a ← adj
-        c ← option Nothing (Just ⦷ np ⧻ pp ⧻ conjp np)
-        return $ maybe a (\c → Phrase A a c) c
+instance Show (X head) where
+    show (X s) = s
 
-conjp ∷ ReadP (Phrase String) → ReadP (Phrase String)
-conjp phrase = do
-  p₁ ← phrase
-  let h = case p₁ of
-            Phrase h _ _ → h
-            Leaf h _     → h
-  c  ← conj
-  p₂ ← phrase +≫ conjp phrase
-  return $ Phrase h p₁ (Phrase Con c p₂)
 
-cp ∷ ReadP (Phrase String)
-cp = do
-  h ← comp
-  b ← vbar
-  return $ Phrase C h b
-
-s ∷ ReadP (Phrase String)
-s = do
-  subj ← np +≫ conjp np
-  vp ← vbar
-  optional (char '.')
-  -- eof
-  return $ Phrase V subj vp
-
-vbar ∷ ReadP (Phrase String)
-vbar = vbar₁ +≫ vbar₂
-    where
-      vbar₁ = do
-        h ← verb
-        b ← option Nothing (Just ⦷ np ⧻ conjp np ⧻ pp)
-        return $ maybe h (Phrase V h) b
-
-      vbar₂ = do
-        b ← np ⧻ conjp np ⧻ pp
-        h ← verb
-        return (Phrase V b h)
-
-pp ∷ ReadP (Phrase String)
-pp = do
-  h ← prep
-  b ← np
-  return (Phrase P h b)
+----------------------
+-- Parsing helpers. --
+----------------------
 
 token ∷ ReadP String
 token = do
@@ -151,6 +131,7 @@ token = do
   skipSpaces
   return t
 
+
 tstring ∷ String → ReadP String
 tstring s = do
   result ← string s
@@ -158,38 +139,272 @@ tstring s = do
   skipSpaces
   return result
 
-non ∷ ReadP a → ReadP ()
+
+non ∷ ReadP α → ReadP ()
 non unwanted = do
   result ← readP_to_S unwanted ⦷ look
   guard (null result)
 
-noun ∷ ReadP (Phrase String)
-noun = non (comp ⧻ prep ⧻ verb ⧻ det) ≫ Leaf N ⦷ token
 
-comp ∷ ReadP (Phrase String)
-comp = Leaf C ⦷ choice (tstring ⦷ comps)
-    where comps = words "that"
+runParser ∷ ReadP α → ReadP α
+runParser parser = do
+  parse ← parser
+  eof
+  return parse
 
-prep ∷ ReadP (Phrase String)
-prep = Leaf P ⦷ choice (tstring ⦷ preps)
-    where preps = words "in with to at"
 
-verb ∷ ReadP (Phrase String)
-verb = Leaf V ⦷ choice (tstring ⦷ verbs)
+-------------------
+-- Coordination. --
+-------------------
+
+type CoordP   = XP 'Coord
+type CoordBar = XBar 'Coord
+type CoordLex = X 'Coord
+
+coord ∷ ReadP CoordLex
+coord =  X ⦷ choice (tstring ⦷ coords)
+    where coords = words "and or"
+
+
+------------------------
+-- Tense projections. --
+------------------------
+
+type IP   = XP 'I
+type IBar = XBar 'I
+type ILex = X 'I
+
+ip ∷ ReadP IP
+ip = do
+  spec  ← (Just ∘ Specifier₂ ⦷ np) ⧻ return Nothing
+  ihead ← ibar
+  return $ maybe (XPₛ (Specifier₀ (X "trace")) ihead) (`XPₛ` ihead) spec
+
+ibar ∷ ReadP IBar
+ibar = do
+  -- This always leaves the specifier and adjunct empty.
+  complement ← vp -- ⧻ cp ∷ ∀h . ReadP (XP h)
+  return $ XBarₘ tense [Complement₂ complement]
+
+tense ∷ ILex
+tense = X ""
+
+
+-------------------------
+-- Verbal projections. --
+-------------------------
+
+type VP   = XP 'V
+type VBar = XBar 'V
+type VLex = X 'V
+
+
+vp ∷ ReadP VP
+vp = do
+  -- This always leaves the specifier position empty.
+  vhead ← vbar
+  return $ XP vhead
+
+
+vbar ∷ ReadP VBar
+vbar = do
+  vhead ← verb
+  complement ← option Nothing (Just ⦷ (Complement₂ ⦷ np) ⧻ (Complement₂ ⦷ cp) ⧻ (Complement₂ ⦷ pp))
+  return $ maybe (XBar vhead) (\comp → XBarₘ vhead [comp]) complement
+
+
+verb ∷ ReadP VLex
+verb = X ⦷ choice (tstring ⦷ verbs)
     where verbs = words "is belonged kept crowed woke married \
                         \kissed milked tossed worried killed ate lay built"
 
-adj ∷ ReadP (Phrase String)
-adj = Leaf A ⦷ choice (tstring ⦷ adjs)
+
+--------------------------
+-- Nominal projections. --
+--------------------------
+
+type NP   = XP N
+type NBar = XBar N
+type NLex = X N
+
+
+np ∷ ReadP NP
+np = (np ⧻ nps)
+    where
+      np = do
+        spec ← option Nothing (fmap (Just ∘ Specifier₀) det)
+        head ← nbar
+        return $ maybe (XP head) (`XPₛ` head) spec
+
+      nps = do
+        l ← np
+        c ← coord
+        r ← np ⧻ nps
+        return $ XPₐ l c r
+
+
+nbar ∷ ReadP NBar
+nbar = withAdj ⧻ nbarʹ
+    where
+      nbarʹ = do
+        nhead ← noun
+        complement ← many $ (Complement₂ ⦷ pp) ⧻
+                            (Complement₂ ⦷ cp) ⧻
+                            (Complement₂ ⦷ adjp)
+        return $ if null complement
+                 then XBar nhead
+                 else XBarₘ nhead complement
+
+      withAdj ∷ ReadP NBar
+      withAdj = do
+        adjunct ← Adjunct₂ ⦷ adjp
+        nhead ← nbar
+        return $ XBarₐʹ adjunct nhead
+
+
+noun ∷ ReadP NLex
+noun = non ((Complement₀ ⦷ det) ⧻
+            (Complement₀ ⦷ adj) ⧻
+            (Complement₀ ⦷ prep) ⧻
+            (Complement₀ ⦷ adv) ⧻
+            (Complement₀ ⦷ verb) ⧻
+            (Complement₀ ⦷ conj) ⧻
+            (Complement₀ ⦷ coord)) ≫ X ⦷ token
+  -- For now assume that a noun is negatively defined by the other
+  -- word classes.
+
+
+-----------------------------
+-- Adjectival projections. --
+-----------------------------
+
+type AdjP   = XP 'Adj
+type AdjBar = XBar 'Adj
+type AdjLex = X 'Adj
+
+adjp ∷ ReadP AdjP
+adjp = adjpʹ ⧻ adjps
+
+    where
+      adjpʹ = do
+        spec ← option Nothing (Just ∘ Specifier₀ ⦷ adv)
+        adjHead ← adjBar
+        return $ maybe (XP adjHead) (`XPₛ` adjHead) spec
+
+      adjps = do
+        l ← adjpʹ
+        c ← coord
+        r ← adjpʹ ⧻ adjps
+        return $ XPₐ l c r
+
+adjBar ∷ ReadP AdjBar
+adjBar = do
+  adjHead ← adj
+  complement ← option Nothing (Just ∘ Complement₂ ⦷ np)
+  return $ maybe (XBar adjHead) (\comp → XBarₘ adjHead [comp]) complement
+
+adj ∷ ReadP AdjLex
+adj = X ⦷ choice (tstring ⦷ adjs)
     where adjs = words "sowing shaven shorn tattered torn forlorn crumpled"
 
-det ∷ ReadP (Phrase String)
-det = Leaf D ⦷ choice (tstring ⦷ dets)
+
+----------------------------
+-- Adverbial projections. --
+----------------------------
+
+type AdvP   = XP 'Adv
+type AdvBar = XBar 'Adv
+type AdvLex = X 'Adv
+
+adv ∷ ReadP AdvLex
+adv = X ⦷ choice (tstring ⦷ advs)
+    where advs = words "all"
+
+--------------------------------
+-- Prepositional projections. --
+--------------------------------
+
+type PP   = XP 'P
+type PBar = XBar 'P
+type PLex = X 'P
+
+
+pp ∷ ReadP (XP 'P)
+pp = do
+  -- This always leaves the AP in specifier position empty.
+  p ← prep
+  arg ← np
+  return $ XP (XBarₘ p [Complement₂ arg])
+
+
+prep ∷ ReadP PLex
+prep = X ⦷ choice (tstring ⦷ preps)
+    where preps = words "in with to at"
+
+
+-----------------------------
+-- Determiner projections. --
+-----------------------------
+
+type DetLex = X Det
+
+
+det ∷ ReadP (X Det)
+det = X ⦷ choice (tstring ⦷ dets)
     where dets = words "a an the some many all his"
 
-conj ∷ ReadP (Phrase String)
-conj = Leaf Con ⦷ choice (tstring ⦷ conjs)
-    where conjs = words "and or"
+
+---------------------------------
+-- Complementizer projections. --
+---------------------------------
+
+type CP   = XP 'Subord
+type CBar = XBar 'Subord
+type CLex = X 'Subord
+
+cp ∷ ReadP CP
+cp = do
+  -- This leaves the specifier always empty.
+  chead ← cbar
+  return $ XP chead
+
+cbar ∷ ReadP CBar
+cbar = do
+  chead ← conj
+  arg   ← ip
+  return $ XBarₘ chead [Complement₂ arg]
+
+conj ∷ ReadP CLex
+conj = X ⦷ choice (tstring ⦷ conjs)
+    where conjs = words "that"
+
+
+
+
+------------------
+
+-- comp ∷ ReadP (Phrase String)
+-- comp = Leaf C ⦷ choice (tstring ⦷ comps)
+--     where comps = words "that"
+
+-- prep ∷ ReadP (Phrase String)
+-- prep = Leaf P ⦷ choice (tstring ⦷ preps)
+--     where preps = words "in with to at"
+
+-- verb ∷ ReadP (Phrase String)
+-- verb = Leaf V ⦷ choice (tstring ⦷ verbs)
+--     where verbs = words "is belonged kept crowed woke married \
+--                         \kissed milked tossed worried killed ate lay built"
+
+-- adj ∷ ReadP (Phrase String)
+-- adj = Leaf A ⦷ choice (tstring ⦷ adjs)
+--     where adjs = words "sowing shaven shorn tattered torn forlorn crumpled"
+
+
+
+-- conj ∷ ReadP (Phrase String)
+-- conj = Leaf Con ⦷ choice (tstring ⦷ conjs)
+--     where conjs = words "and or"
 
 (⧻) ∷ ReadP α → ReadP α → ReadP α
 (⧻) = (+++)
@@ -198,3 +413,7 @@ infixr 5 ⧻
 (<⧺) ∷ ReadP α → ReadP α → ReadP α
 (<⧺) = (<++)
 infixr 5 <⧺
+
+(⧺>) ∷ ReadP α → ReadP α → ReadP α
+(⧺>) = flip (<⧺)
+infixl 5 ⧺>
