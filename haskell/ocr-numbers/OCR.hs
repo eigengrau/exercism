17fa95f8cd66af5afb0eba3be737f79201f0d206
Sebastian Reuße <seb@wirrsal.net>
2015-09-09 17:38:45 +0200

    Update.

--- haskell/ocr-numbers/OCR.hs
+++ haskell/ocr-numbers/OCR.hs
@@ -19,6 +19,7 @@ import Control.Monad.Unicode
 import Test.QuickCheck.Arbitrary
 import Test.QuickCheck hiding (output)
 import Control.Monad
+import Statistics.Sample
 
 
 convert ∷ String → String
@@ -29,10 +30,20 @@ convert input = intercalate "," $ do
 
 convert1 ∷ String → String
 convert1 input = unsafePerformIO $ do
-                   result ← return ∘ ((`runNet` input) =≪) =≪ makeNet
+                   result ←
+                       return ∘ ((`runNet` input) =≪) =≪ makeNet
                    return $ case result of
-                              Left  err → err
-                              Right i   → show i
+                              Left  err     → err
+                              Right (i,[_,_,skew]) → if skew < 0.3
+                                                     then "?"
+                                                     else show i
+
+comparison = do
+  net ← makeNet
+  let normal = fmap (fmap snd) (mapM (\c → net ≫= (`runNet` c)) font)
+      garbled = fmap (fmap snd) (mapM (\c → net ≫= (`runNet` c)) garble)
+  return ((,) ⦷ normal ⊛ garbled)
+
 
 evalNet ∷ IO (Either String Int)
 evalNet = do
@@ -44,10 +55,12 @@ evalNet = do
   return $ fmap maxIndex results
     where inputs = fmap (fmap fst) digitSamples
 
-runNet ∷ Network Double → String → Either String Int
+runNet ∷ Network Double → String → Either String (Int, [Double])
 runNet net s = do
-  input ←  encodeInput s
-  return ∘ maxIndex ∘ toList $ output net tanh input
+  input ← encodeInput s
+  let result = output net tanh input
+  return (maxIndex (toList result),
+          [kurtosis result, stdDev result, skewness result])
 
 testNet ∷ Network Double → String → Either String [Double]
 testNet net s = do
@@ -60,32 +73,20 @@ makeNet = do
   let netʹ = fmap (train net) digitSamples
   return netʹ
       where
-        initialNet = createNetwork 16 [12] 11
-        -- train = trainUntilErrorBelow 6 learnRate  tanh tanh'
-        train = trainNTimes 10000 learnRate  tanh tanh'
-        learnRate  = 0.1
+        initialNet = createNetwork 16 [12,12] 10
+        train = trainUntilErrorBelow 5 learnRate  tanh tanh'
+        -- train = trainNTimes 10000 learnRate  tanh tanh'
+        learnRate  = 0.3
 
 -- Encode the digit font as a numeric vector and pair these up with target
 -- vectors to be learned.
 digitSamples ∷ Either String (Samples Double)
-digitSamples = do
-  samples ← zip ⦷ sequence inputs ⊛ pure targets
-  g ← garbled
-  return (samples ⧺ g)
+digitSamples = zip ⦷ sequence inputs ⊛ pure targets
 
     where
       inputs  = fmap encodeInput font
-      targets = fmap fromList ∘ take 10 ∘ fmap (⧺ [0]) $
-                  iterate rotate (1 : replicate 9 0)
-                          -- Always leave the last output 0. This will be used
-                          -- to detect garbled input.
+      targets = fmap fromList ∘ take 10 $ iterate rotate (1 : replicate 9 0)
       rotate l = last l : init l
-      garbled ∷ Either String [(Vector Double, Vector Double)]
-      garbled =  zip ⦷
-                   (sequence ∘ unsafePerformIO $ replicateM 10 makeGarbleVector) ⊛
-                   pure (repeat garbledTarget)
-      garbledTarget ∷ Vector Double
-      garbledTarget = fromList $ replicate 10 0 ⧺ [1]
 
 encodeInput ∷ String → Either String (Vector Double)
 encodeInput s
@@ -131,11 +132,30 @@ test = unlines
                                , "  ||_||_|"
                                , "  ||_| _|"
                                , "         " ]
-garble = unlines [
-          "   "
+garble = [unlines [
+           "   "
           , "| |"
           , "| |"
           , "   "
+          ],
+          unlines [
+           " _ "
+          ,"| |"
+          ," _|"
+          ,"   "
+          ],
+          unlines [
+           "   "
+          ,"| |"
+          ,"|_|"
+          ,"   "
+          ],
+          unlines [
+           "   "
+          ,"  |"
+          ,"  |"
+          ,"   "
+          ]
          ]
 
 makeGarbleVector ∷ IO (Either String (Vector Double))
@@ -149,3 +169,48 @@ makeGarble = do
 
         where
           digitElem = elements "|_ "
+
+-- Garbled
+    -- [[- 1.5441037975666287, 0.29330325405980856, - 0.2260091809091274,
+    --   0.11723915215092957],
+    --  [- 1.4732096692291283, 0.3245441592306161, 4.950756998173012e-2,
+    --   4.7804026233086785e-2],
+    --  [- 1.3122005521609603, 0.28991121525613256, 0.44263129640128857,
+    --   0.17089298584051682],
+    --  [0.1285626045970818, 0.34271021441644595, - 1.0188292255815243,
+    --   4.937116638166379e-2],
+    --  [- 1.2974311719339153, 0.2630574271281181, 0.28588649533692656,
+    --   0.11625291941181826],
+    --  [- 1.3763423449496064, 0.28204540188843374, 2.6822776105237964e-2,
+    --   0.11439089510965957],
+    --  [- 1.0218224710322605, 0.287737622429675, - 0.4171014265145352,
+    --   6.447778721893596e-2],
+    --  [- 1.0494324095001377, 0.3100839049851633, - 0.13064852568674057,
+    --   7.760472195892168e-2],
+    --  [- 0.901716583951651, 0.33550620418363863, - 0.3328680973460485,
+    --   5.975566043899061e-2],
+    --  [- 0.36261148053709613, 0.3095065930143726, - 0.7406564137487083,
+    --   8.661348031570903e-2]]
+
+
+-- Ungarbled 
+    -- [[0.9086649225282404, 0.2506753790746807, 1.2701933631112652,
+    --   7.898865722165958e-2],
+    --  [1.6537449428539146, 0.24764889448608832, 1.4875067952479646,
+    --   8.81243266806404e-2],
+    --  [3.982172609718612, 0.25931462924658555, 2.3077314293634643,
+    --   9.732240674539154e-2],
+    --  [2.9951925801246944, 0.2668305414659457, 2.000031631029371,
+    --   7.395352262079175e-2],
+    --  [2.6467907907900257, 0.2412244361629522, 1.6603937664390742,
+    --   7.37352615667335e-2],
+    --  [0.17196427685012505, 0.25189782269293365, 0.6461747750249938,
+    --   7.363447093796949e-2],
+    --  [- 0.3944685229216942, 0.24902611755424897, 0.6839705863044155,
+    --   9.982194718475182e-2],
+    --  [3.9432702885830127, 0.2884286637614056, 2.2769028628822943,
+    --   9.41361011126425e-2],
+    --  [- 0.19622540655365306, 0.22133785664290948, 0.19556183875173613,
+    --   0.1113090419757485],
+    --  [2.8186089787045203, 0.24844319612347443, 1.8458664012483963,
+    --   6.282678607447303e-2]]
