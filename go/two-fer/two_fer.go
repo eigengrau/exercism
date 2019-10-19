e1db4121e4323d65179da918c27a8275bf4ef2c4
Sebastian Reuße <seb@wirrsal.net>
2019-10-19 12:52:48 +0200

    Update

--- go/two-fer/two_fer.go
+++ go/two-fer/two_fer.go
@@ -5,16 +5,11 @@ import (
 	"fmt"
 )
 
-var template = "One for %s, one for me."
-
 // ShareWith returns a two-fer sentence with the beneficiary chosen based on
 // name.
 func ShareWith(name string) string {
-	var beneficiary string
 	if name == "" {
-		beneficiary = "you"
-	} else {
-		beneficiary = name
+		name = "you"
 	}
-	return fmt.Sprintf(template, beneficiary)
+	return fmt.Sprintf("One for %s, one for me.", name)
 }
