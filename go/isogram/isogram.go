8cac78404972e190f4c66812b19ae27c7717ac67
Sebastian Reuße <seb@wirrsal.net>
2019-10-20 09:43:18 +0200

    Update

--- go/isogram/isogram.go
+++ go/isogram/isogram.go
@@ -1,1 +1,24 @@
+// Package isogram checks for isogrammaticity.
 package isogram
+
+import (
+	"strings"
+	"unicode"
+)
+
+type set map[rune]interface{}
+
+func IsIsogram(w string) bool {
+	w = strings.ToLower(w)
+	seen := make(set)
+	for _, c := range w {
+		if !unicode.IsLetter(c) {
+			continue
+		}
+		if _, ok := seen[c]; ok {
+			return false
+		}
+		seen[c] = nil
+	}
+	return true
+}
