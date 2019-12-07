a3096b56ba348f2eebb9cef1190f27387d0111b7
Sebastian Reuße <seb@wirrsal.net>
2019-12-07 16:28:01 +0100

    Update

--- go/grains/grains.go
+++ go/grains/grains.go
@@ -5,37 +5,30 @@ import (
 	"fmt"
 )
 
-// memo stores powers of two.
-var memo [64]uint64 = [64]uint64{1}
-
-// needle keeps the offset to first non-memoized item.
-var needle int = 1
-
-// Square calculates 2ⁿ.
+// Square calculates 2ⁿ⁻¹.
 func Square(n int) (uint64, error) {
 	if n <= 0 || n > 64 {
 		return 0, fmt.Errorf("expecting 0 > n <= 64")
 	}
-	// square expects offsets into the memo table, which are 0-based, whereas
-	// Square offsets are 1-based.
+	// Square() inputs are natural numbers (offsets into into a chess-board),
+	// whereas square() assumes the input is integral.
 	return square(n - 1), nil
 }
 
-// square calculates 2ⁿ⁺¹ and memoizes intermediate results.
+// square calculates 2ⁿ.
 func square(n int) uint64 {
-	// Memoize all missing in-between values.
-	for ; needle <= n; needle++ {
-		memo[needle] = 2 * memo[needle-1]
+	if n == 0 {
+		return 1
 	}
-	return memo[n]
+	return 2 << (n - 1)
 }
 
-// Total calculates Σ2¹+…+2⁶⁴.
+// Total calculates Σ2⁰+…+2⁶³.
 func Total() uint64 {
 	// return 18446744073709551615 ;-)
-	var sum uint64
+	var sum uint64 = 1
 	for i := 0; i < 64; i++ {
-		sum += square(i)
+		sum += 2 << i
 	}
 	return sum
 }
