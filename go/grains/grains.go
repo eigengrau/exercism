0a1b65fe63af9cd99a6531134c2a3366c64e1e92
Sebastian Reuße <seb@wirrsal.net>
2019-12-07 18:15:07 +0100

    Update

--- go/grains/grains.go
+++ go/grains/grains.go
@@ -3,6 +3,7 @@ package grains
 
 import (
 	"fmt"
+	"math"
 )
 
 // Square calculates 2ⁿ⁻¹.
@@ -10,25 +11,13 @@ func Square(n int) (uint64, error) {
 	if n <= 0 || n > 64 {
 		return 0, fmt.Errorf("expecting 0 > n <= 64")
 	}
-	// Square() inputs are natural numbers (offsets into into a chess-board),
-	// whereas square() assumes the input is integral.
-	return square(n - 1), nil
-}
-
-// square calculates 2ⁿ.
-func square(n int) uint64 {
-	if n == 0 {
-		return 1
+	if n == 1 {
+		return 1, nil
 	}
-	return 2 << (n - 1)
+	return 2 << (n - 2), nil
 }
 
-// Total calculates Σ2⁰+…+2⁶³.
+// Total returns Σ2⁰+…+2⁶³.
 func Total() uint64 {
-	// return 18446744073709551615 ;-)
-	var sum uint64 = 1
-	for i := 0; i < 64; i++ {
-		sum += 2 << i
-	}
-	return sum
+	return math.MaxUint64
 }
